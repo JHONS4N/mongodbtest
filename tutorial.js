3aba3ff02ff946789ccb3bfaabe99d486b3e08a3
Jhon Casallas <jhoncasallas@Jhons-MacBook-Pro.local>
2020-01-02 17:49:01 -0500

    start project

---
+++ tutorial.js
@@ -0,0 +1,65 @@
+const {MongoClient}=require ('mongodb');
+
+async function main (){
+    const uri ="mongodb+srv://S4NAdmin:S4NAdmin@s4ntest-dtujr.mongodb.net/test?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri,{useNewUrlParser:true,useUnifiedTopology:true})
+    try{
+        await client.connect();
+        /* await createListing(client,{
+            name:"Lovely loft",
+            summary:"A charming loft in Paris",
+            bedrooms:1,
+            bathrooms:1
+        }); */
+
+        await createdMultipleListings(client,[
+        {
+            name:"Infinite Views",
+            summary:"Modern home",
+            property_type:"House",
+            bedrooms:1,
+            bathrooms:1,
+            beds:5
+        },
+        {
+            name:"Private room in london",
+            property_type:"Apartament",
+            bedrooms:5,
+            bathrooms:4.5,
+            beds:5
+        },
+        {
+            name:"Beautiful beach House",
+            summary:"Enjoy relaxed beach",
+            bedrooms:4,
+            bathrooms:2.5,
+            beds:1,
+            last_review: new Date()
+        },
+    ]);
+
+    }catch(e){
+        console.error(e);
+    }
+    finally{
+        await client.close();
+    }
+}
+main().catch(console.err);
+
+async function createdMultipleListings(client,newListings){
+    const result =await  client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListings)
+console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+console.log(result.insertedIds)
+}
+async function createListing(client,newListing){
+const result =await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+console.log(`New Listing created with the following id: ${result.insertedId}`);
+}
+async function listDatabases(client){
+    const databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+
+}
